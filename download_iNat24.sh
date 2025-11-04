c1da2a8e6b9f4bc2cdc7540b91a5ee94d6451bef
mtanneau3 <mtanneau3@gatech.edu>
2025-11-04 13:45:47 -0500

    Fix typo in download script

--- download_iNat24.sh
+++ download_iNat24.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-uvx gdown 1c1FsTKXn45lxgL8lLCM-IvB1kAXEkujo --folder -O data/iNat24
-uvx gdown 1frfkLvrL_CJjOnrgx9DhWDarERQflAuH --folder -O data/iNat24
-uvx gdown 1FGIQnyS201zd5le0MxLihHCbUwuzZTJd --folder -O data/iNat24
-uvx gdown 1VMBuA1KbSItKOV2kh1ag6RnUrQvdkEvS --folder -O data/iNat24
-uvx gdown 1udOfC-J_p0l2_2aAKx3vuSoe9Ap-6FXc --folder -O data/iNat24
-uvx gdown 1JW-Z24zbcBuCb5bGRxMECUoIxH8Kmg__ --folder -O data/iNat24
-uvx gdown 1QABd-7VpjzaOP7v1Kbf6Rgmgd5ujkArR --folder -O data/iNat24
-uvx gdown 1j5chxOkYq8WWsnFL8parpJwPXio-RkRX --folder -O data/iNat24
-uvx gdown 1KG5rSTa_GZiJnb-BiD-5Mc9jnX_jeolU --folder -O data/iNat24
+uvx gdown 1c1FsTKXn45lxgL8lLCM-IvB1kAXEkujo -O data/iNat24/
+uvx gdown 1frfkLvrL_CJjOnrgx9DhWDarERQflAuH -O data/iNat24/
+uvx gdown 1FGIQnyS201zd5le0MxLihHCbUwuzZTJd -O data/iNat24/
+uvx gdown 1VMBuA1KbSItKOV2kh1ag6RnUrQvdkEvS -O data/iNat24/
+uvx gdown 1udOfC-J_p0l2_2aAKx3vuSoe9Ap-6FXc -O data/iNat24/
+uvx gdown 1JW-Z24zbcBuCb5bGRxMECUoIxH8Kmg__ -O data/iNat24/
+uvx gdown 1QABd-7VpjzaOP7v1Kbf6Rgmgd5ujkArR -O data/iNat24/
+uvx gdown 1j5chxOkYq8WWsnFL8parpJwPXio-RkRX -O data/iNat24/
+uvx gdown 1KG5rSTa_GZiJnb-BiD-5Mc9jnX_jeolU -O data/iNat24/
