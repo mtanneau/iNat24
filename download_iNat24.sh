c9a640e5d11d79449e2186a66211ab15839df72d
mtanneau3 <mtanneau3@gatech.edu>
2025-11-04 13:43:40 -0500

    Script to download iNat24 embedding data

---
+++ download_iNat24.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+uvx gdown 1c1FsTKXn45lxgL8lLCM-IvB1kAXEkujo --folder -O data/iNat24
+uvx gdown 1frfkLvrL_CJjOnrgx9DhWDarERQflAuH --folder -O data/iNat24
+uvx gdown 1FGIQnyS201zd5le0MxLihHCbUwuzZTJd --folder -O data/iNat24
+uvx gdown 1VMBuA1KbSItKOV2kh1ag6RnUrQvdkEvS --folder -O data/iNat24
+uvx gdown 1udOfC-J_p0l2_2aAKx3vuSoe9Ap-6FXc --folder -O data/iNat24
+uvx gdown 1JW-Z24zbcBuCb5bGRxMECUoIxH8Kmg__ --folder -O data/iNat24
+uvx gdown 1QABd-7VpjzaOP7v1Kbf6Rgmgd5ujkArR --folder -O data/iNat24
+uvx gdown 1j5chxOkYq8WWsnFL8parpJwPXio-RkRX --folder -O data/iNat24
+uvx gdown 1KG5rSTa_GZiJnb-BiD-5Mc9jnX_jeolU --folder -O data/iNat24
